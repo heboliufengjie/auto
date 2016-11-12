f3de1fc896dfc7a6dd92190f5de74a4a703cf24c
375892795@qq.com <heboliufengjie@126.com>
2016-11-12 23:43:17 +0800

    1

--- myblog/index.js
+++ myblog/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('hello, express');
+    res.send('hello, express 刘凤杰制学习作');
 });
 
 app.listen(3000);
