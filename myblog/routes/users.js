c6a15341621ff9d6fa255a5232d665d72d2b7515
375892795@qq.com <heboliufengjie@126.com>
2016-11-13 00:07:06 +0800

    add ejs

--- myblog/routes/users.js
+++ myblog/routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.send('hello, express user');
+router.get('/:name', function(req, res) {
+    res.render('users', {
+        name: req.params.name
+    });
 });
 
 module.exports = router;
