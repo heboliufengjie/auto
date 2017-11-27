bbbba8410e984c7b197ca072c4728d6658393544
fengjie.liu <fengjie.liu@chinascopefinancial.com>
2017-11-27 18:24:21 +0800

    'add'

--- z_avalon/src/main.js
+++ z_avalon/src/main.js
@@ -1,13 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+//var avalon = require('./src2/avalon');\
+import avalon from './src2/avalon'
 
-Vue.config.productionTip = false
+var avalon1 = require('./src/avalon');
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+
+avalon.define({
+  $id: 'test',
+  aaa: 'aaa',
+  bbb: 'bbb'
 })
---
+++ z_avalon/src/src/avalon.js
@@ -0,0 +1,22 @@
+// import {avalon} from './seed/core'
+// import './seed/lang.compact'
+
+
+// import './filters/index'
+// import './dom/compact'
+
+// import './vtree/fromString'
+// import './vtree/fromDOM'
+
+// import './vdom/compact'
+// import './vmodel/compact'
+// import './directives/compact'
+
+// import './renders/domRender'
+
+// import './effect/index'
+// import './component/index'
+//export default avalon
+
+var avalon={};
+export default avalon
